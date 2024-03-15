a42770122db7ebdd2f1e0b20b66644bd82134f2f
Anosy0 <lucas.theodore2004@gmail.com>
2024-03-15 16:54:35 +0100

    Add Dockerfile

--- Stockage/src/main/java/org/example/Main.java
+++ Stockage/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        org.example.ports.StorageService storageService = new StorageService();
+        StorageService storageService = new StorageService();
         StockManager stockManager = new StockManager(storageService);
 
         // Test adding and removing products
@@ -17,7 +17,7 @@ public class Main {
 
         // Print current stock
         System.out.println("Current stock:");
-        for (Map.Entry<String, Integer> entry : ((StorageService) storageService).getStock().entrySet()) {
+        for (Map.Entry<String, Integer> entry : storageService.getStock().entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
